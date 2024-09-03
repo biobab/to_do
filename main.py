db39735efc293f166c173b46e7772084d480e2f7
abel <abelszemler.off@gmail.com>
2024-09-03 17:37:05 +0200

    add select item

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # from typing import Optional #workswith ex.: age: int|None
-from sqlmodel import Field, SQLModel, create_engine, Session
+from sqlmodel import Field, SQLModel, create_engine, Session, select
 from datetime import datetime, UTC
 import dotenv
 import os
@@ -15,22 +15,30 @@ class Items(SQLModel, table=True):
 
 dotenv.load_dotenv()
 url = os.environ["DATABASE_URL"]
-engine = create_engine(url, echo=True)
+engine = create_engine(url)
 
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-def create_item(name):
-    item = Items(name=name)
-    session = Session(engine)
-    with session:
+def create_item(new_item):
+    item = Items(name=new_item)
+    with Session(engine) as session:
         session.add(item)
         session.commit()
     # session.close() -> a with session miatt nem kell
 
 
+def select_item(x):
+    with Session(engine) as session:
+        statement = select(Items).where(Items.id == x)
+        result = session.exec(statement)
+        item = result.one()
+        print(item)
+
+
 if __name__ == "__main__":
     create_db_and_tables()
-    create_item(" ,@/'' ")
+    # create_item("-.-")
+    select_item(1)
